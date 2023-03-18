af547e4c034e54eacdfc4ee2221e4aa614b31401
Hung Nguyen <hung.nguyen@vogappdevelopers.com>
2023-03-19 00:53:43 +0700

    Initial Commit

---
+++ MovieApp/MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by HungNguyen on 2023/03/19.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
